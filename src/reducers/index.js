c34df0f0d3e6a5221225e6579c50552c36a501a3
Tagor Petrosian <p.tagor@gmail.com>
2018-08-01 20:25:07 +0300

    Added reducer-posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineReducers } from 'redux';
-
+import { combineReducers } from "redux";
+import PostsReducer from "./reducer-posts";
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer
 });
 
 export default rootReducer;
