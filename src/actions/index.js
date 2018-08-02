fd340726352d2b04314e138516889cf3d3feadb7
Tagor Petrosian <p.tagor@gmail.com>
2018-08-02 21:48:04 +0300

    Added post-show component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { API_KEY } from "../config/keys";
+import rootReducer from "../reducers";
 
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
+export const FETCH_POST = "FETCH_POST";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 export function fetchPosts() {
@@ -21,6 +23,15 @@ export function createPost(values, callback) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
