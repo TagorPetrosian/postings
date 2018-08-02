91e4a5edf310d5de6f695e197e2f579ec35cb6c9
Tagor Petrosian <p.tagor@gmail.com>
2018-08-02 18:43:19 +0300

    Added form with validaiton to posts-new component

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,13 +1,63 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
     return (
-      <div>
-        <h3>PostsNew</h3>
+      <div className="from-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.touched ? field.meta.error : ""}
       </div>
     );
   }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    // handleSubmit is a property being passed to the component on behalf redux-form
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field label="Tags" name="tags" component={this.renderField} />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary m-t-1">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+// Runs automatically by redux forms
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+
+  if (!values.tags) {
+    errors.tags = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+  return errors;
 }
 
-export default PostsNew;
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(PostsNew);
